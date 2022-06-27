579c5eb290fc4a5263159cb1e626877a80a91885
Jonathan Tanisaka <jktanisaka@gmail.com>
2022-06-27 22:49:39 +0000

    cleaned up app.jsx and started hook for state

--- client/pages/home.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Level1 from './level-1';
-
-export default function Home(props) {
-  return (
-   <Level1 />
-  );
-}
--- client/pages/level-1.jsx
+++ client/pages/level-1.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 export default function Level1(Props) {
-  // const [counter, setCounter] = useState(0);
-
+  const [counter, setCounter] = useState(0);
+  const increment = () => {
+    setCounter(counter + 1);
+  };
   return (
   <div className="x">
-      <button className="y"></button>
+      <button className="y" onClick={increment}></button>
   </div>
   );
 }
