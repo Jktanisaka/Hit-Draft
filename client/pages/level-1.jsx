8a8dcb896cfb627e3e15489088c8c3d47e9eb25e
Jonathan Tanisaka <jktanisaka@gmail.com>
2022-06-27 22:02:31 +0000

    completed setup and made button

---
+++ client/pages/level-1.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Level1(Props) {
+  // const [counter, setCounter] = useState(0);
+
+  return (
+  <div className="x">
+      <button className="y"></button>
+  </div>
+  );
+}
