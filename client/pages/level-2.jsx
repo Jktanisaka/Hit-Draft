afbf5212e95936fefd29897135f507cc0d747890
Jonathan Tanisaka <jktanisaka@gmail.com>
2022-06-28 03:56:41 +0000

    completed random location for level-2

--- client/pages/level-2.jsx
+++ client/pages/level-2.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Level2(props) {
+  const [counter, setCounter] = useState(0);
+  const [topDimension, setTopDimension] = useState(100);
+  const [leftDimension, setLeftDimension] = useState(100);
+
+  const increment = () => {
+    setCounter(counter + 1);
+  };
+
+  const handleEnter = () => {
+    setTopDimension(Math.random() * 1000);
+    setLeftDimension(Math.random() * 1000);
+  };
+
   return (
     <div style={{ position: 'relative' }}>
-      <button style={{ top: 100, left: 100, position: 'absolute' }}></button>
+      <button style={{ top: topDimension, left: leftDimension, position: 'absolute' }} onMouseEnter={handleEnter} onClick={increment}></button>
     </div>
   );
 }
