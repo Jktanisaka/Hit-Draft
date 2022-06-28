845a0cc6c1afd21f8d0af1f5361da86adf2afd99
Jonathan Tanisaka <jktanisaka@gmail.com>
2022-06-28 03:05:08 +0000

    completed level-1

--- client/pages/level-1.jsx
+++ client/pages/level-1.jsx
@@ -1,9 +1,15 @@
 import React, { useState } from 'react';
+import Level2 from './level-2';
+
 export default function Level1(Props) {
-  const [counter, setCounter] = useState(0);
+  const [counter, setCounter] = useState(false);
   const increment = () => {
-    setCounter(counter + 1);
+    setCounter(!counter);
   };
+
+  if (counter === true) {
+    return <Level2 />;
+  }
   return (
   <div className="x">
       <button className="y" onClick={increment}></button>
