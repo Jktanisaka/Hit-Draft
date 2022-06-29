0f265b396b3edf929566fc4c32800649b5dfd225
Jonathan Tanisaka <jktanisaka@gmail.com>
2022-06-29 22:14:44 +0000

    made button expand on click

--- client/pages/level-5.jsx
+++ client/pages/level-5.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default function Level5(props) {
   return (
-  <div className=" container-fluid">
-    <div className='row justify-content-center align-items-center full-height m-0'>
-      <button style={{ height: 100, width: 100 }} className="m-5"></button>
+  <div className="container-fluid p-0">
+    <div className='row justify-content-center align-items-center full-height m-0 p-0'>
+      <a className="asdf"></a>
     </div>
   </div>);
 }
