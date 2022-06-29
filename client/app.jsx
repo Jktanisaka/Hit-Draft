5652588f20f2d0d70db54e4ab6c352eb4db0cb3f
Jonathan Tanisaka <jktanisaka@gmail.com>
2022-06-29 00:23:49 +0000

    completed remaining hooks for level 3

--- client/app.jsx
+++ client/app.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Level1 from './pages/level-1';
+import 'bootstrap/dist/css/bootstrap.min.css';
 export default class App extends React.Component {
   render() {
     return <Level1 />;
