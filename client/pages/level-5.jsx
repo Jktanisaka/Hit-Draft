b177dac1cf981b4f20e918cf2276f95aca8c77da
Jonathan Tanisaka <jktanisaka@gmail.com>
2022-06-30 01:36:18 +0000

    finished rendering new enlarged button

--- client/pages/level-5.jsx
+++ client/pages/level-5.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Level5(props) {
+  const [passed, setPassed] = useState(false);
+  const increment = () => {
+    setPassed(!passed);
+  };
+  if (passed === true) {
+    return (
+      <div className="container-fluid p-0 final">
+
+      </div>
+    );
+  }
   return (
   <div className="container-fluid p-0">
     <div className='row justify-content-center align-items-center full-height m-0 p-0'>
-      <a className="asdf"></a>
+      <a className= "asdf" onMouseUp={increment}></a>
     </div>
   </div>);
 }
