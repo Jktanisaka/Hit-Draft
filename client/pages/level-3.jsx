453446b673ed8f66cd74f0c05fe0c213988ec6f8
Jonathan Tanisaka <jktanisaka@gmail.com>
2022-07-06 00:54:22 +0000

    refactored level-3 to use animation instead of transition

--- client/pages/level-3.jsx
+++ client/pages/level-3.jsx
@@ -4,14 +4,9 @@ import { Link } from 'react-router-dom';
 export default function Level3(props) {
   const reducer = (state, action) => {
     switch (action.type) {
-      case 'passedLevel':
-        return {
-          passedLevel: true
-        };
       case 'hoveredButton':
         return {
-          hoveredButton: true,
-          passedLevel: false
+          hoveredButton: true
         };
       default:
         return state;
@@ -19,23 +14,23 @@ export default function Level3(props) {
   };
   const [state, dispatch] = useReducer(reducer,
     {
-      hoveredButton: false,
-      passedLevel: false
+      hoveredButton: false
     });
 
   if (state.hoveredButton === true) {
     return (
-      <div className="container">
-        <div className='row justify-content-end align-items-center full-height'>
-           <Link className='gray-square l3-button-hidden' to="/level4" />
+      <div className="container position-relative">
+        <div className='row justify-content-center align-items-center fade-out full-height'>
+          <button style={{ height: 100, width: 100 }} className="l3-button shadow-none" disabled></button>
         </div>
+           <Link className='gray-square l3-button-hidden position-absolute' to="/level4" style={{ top: 300, left: 800 }}/>
       </div>
     );
   }
   return (
     <div className="container">
       <div className='row justify-content-center align-items-center full-height'>
-        <button style={{ height: 100, width: 100 }} className="l3-button shadow-none" onMouseLeave={ () => { dispatch({ type: 'hoveredButton' }); }} ></button>
+        <button style={{ height: 100, width: 100 }} className="l3-button shadow-none" onMouseEnter={ () => { dispatch({ type: 'hoveredButton' }); }} ></button>
       </div>
     </div>
   );
