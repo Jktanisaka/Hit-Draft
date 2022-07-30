732d809990eae2ecfc506540ef7391a220312076
Jonathan Tanisaka <jktanisaka@gmail.com>
2022-07-30 08:24:53 +0000

    dogs now stay in place when finishing

--- client/components/results.jsx
+++ client/components/results.jsx
@@ -56,7 +56,7 @@ export default function Results(props) {
     <div className='d-flex justify-content-evenly align-items-center ms-2 me-2'>
            {
             picks.slice(1).map((player, index) => {
-              return (<div key={index} className='col-2 d-flex flex-column focused' style={{ border: 'white', animation: `fade-in ${parseInt(index)}s` }} >
+              return (<div key={index} className='col-2 d-flex flex-column focused' style={{ border: 'white', animation: `animate-fade ${parseInt(index)}s` }} >
                 <h3 className='text-center m-2' style={{ fontSize: 20, display: 'inline' }}>{player[0]}<p style={{ display: 'inline', fontSize: 14 }} className='text-muted'>(#{index + 1})</p></h3>
                 <form className='row justify-content-center mb-3'>
                   <input className='mb-1' style={{ width: '80%' }} tabIndex={parseInt(index + 1)} placeholder='1st Pick' onChange={onChange} data-index={1} player-index={index + 1} value={player[1]} onFocus={handleFocus}></input>
