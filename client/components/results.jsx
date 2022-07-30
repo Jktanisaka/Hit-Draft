ca98c2b5e7e34cea7acd51074b755db1d8030ef3
Jonathan Tanisaka <jktanisaka@gmail.com>
2022-07-30 04:32:18 +0000

    completed adding csv file name

--- client/components/results.jsx
+++ client/components/results.jsx
@@ -1,22 +1,24 @@
 import React, { useState, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { CSVLink } from 'react-csv';
 import { useBeforeunload } from 'react-beforeunload';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
 export default function Results(props) {
+  const navigate = useNavigate();
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const [showCSV, setShowCSV] = useState(false);
+  const handleCloseCSV = () => setShowCSV(false);
+  const handleShowCSV = () => setShowCSV(true);
+
+  const [fileName, setFileName] = useState('');
+
   const { state } = useLocation();
   const winners = state.length;
   const [picks, setPicks] = useState(state);
-
-  // useEffect(() => {
-  //   const winnerArr = [['Name', '1st Pick', '2nd Pick', '3rd Pick', '4th Pick', '5th Pick', '6th Pick', '7th Pick', '8th Pick', '9th Pick', '10th Pick', 'Additional Notes']];
-  //   state.map(winner => (
-  //     winnerArr.push([`${winner.name}`, '', '', '', '', '', '', '', '', '', '', ''])
-  //   ));
-  //   const duplicatePicks = [...picks];
-  //   duplicatePicks.push(winnerArr);
-  //   setPicks(duplicatePicks);
-
-  // }, []);
   useBeforeunload(event => {
 
     localStorage.setItem('winners', JSON.stringify(picks));
@@ -28,7 +30,19 @@ export default function Results(props) {
       setPicks(winners);
     }
   }, []);
-
+  const handleFocus = event => {
+    const selectedDiv = event.target.closest('div');
+    selectedDiv.classList.add('focused');
+  };
+  const handleReset = () => {
+    setPicks([]);
+    localStorage.removeItem('winners');
+    setShow(false);
+    navigate('/');
+  };
+  const fileNameChange = event => {
+    setFileName(event.target.value);
+  };
   const onChange = event => {
     const stateCopy = [...picks];
     const inputIndex = event.target.getAttribute('data-index');
@@ -43,26 +57,55 @@ export default function Results(props) {
            {
             picks.slice(1).map((player, index) => {
               return (<div key={index} className='col-2 d-flex flex-column ' style={{ backgroundColor: 'white', border: `2px solid ${player.color}`, animation: `fade-in ${index + 1}s` }} >
-                <p className='text-center m-2' style={{ fontSize: 20 }}>{player[0]}(#{index + 1})</p>
+                <h3 className='text-center m-2' style={{ fontSize: 20, display: 'inline' }}>{player[0]}<p style={{ display: 'inline', fontSize: 14 }} className='text-muted'>(#{index + 1})</p></h3>
                 <form className='row justify-content-center mb-3'>
-                  <input className='mb-1' style={{ width: '80%' }} tabIndex={parseInt(index + 1)} placeholder='1st Pick' onChange={onChange} data-index={1} player-index={index + 1} value={player[1]} ></input>
-                  <input className='mb-1' style={{ width: '80%' }} tabIndex={(2 * winners) - parseInt(index)} placeholder='2nd Pick' onChange={onChange} data-index={2} player-index={index + 1} value={player[2]} ></input>
-                  <input className='mb-1' style={{ width: '80%' }} tabIndex={(3 - 1) * winners + index + 1} placeholder="3rd Pick" onChange={onChange} data-index={3} player-index={index + 1} value={player[3]}></input>
-                  <input className='mb-1' style={{ width: '80%' }} tabIndex={(4 * winners) - parseInt(index)} placeholder='4th Pick' onChange={onChange} data-index={4} player-index={index + 1} value={player[4]}></input>
-                  <input className='mb-1' style={{ width: '80%' }} tabIndex={(5 - 1) * winners + index + 1} placeholder="5th Pick" onChange={onChange} data-index={5} player-index={index + 1} value={player[5]}></input>
-                  <input className='mb-1' style={{ width: '80%' }} tabIndex={(6 * winners) - parseInt(index)} placeholder='6th Pick' onChange={onChange} data-index={6} player-index={index + 1} value={player[6]}></input>
-                  <input className='mb-1' style={{ width: '80%' }} tabIndex={(7 - 1) * winners + index + 1} placeholder="7th Pick" onChange={onChange} data-index={7} player-index={index + 1} value={player[7]}></input>
-                  <input className='mb-1' style={{ width: '80%' }} tabIndex={(8 * winners) - parseInt(index)} placeholder="8th Pick" onChange={onChange} data-index={8} player-index={index + 1} value={player[8]}></input>
-                  <input className='mb-1' style={{ width: '80%' }} tabIndex={(9 - 1) * winners + index + 1} placeholder="9th Pick" onChange={onChange} data-index={9} player-index={index + 1} value={player[9]}></input>
-                  <input className='mb-1' style={{ width: '80%' }} tabIndex={(10 * winners) - parseInt(index)} placeholder="10th Pick" onChange={onChange} data-index={10} player-index={index + 1} value={player[10]}></input>
-                  <textarea style={{ width: '80%' }} placeholder='Additional Notes' onChange={onChange} data-index={11} player-index={index + 1} value={player[11]}></textarea>
+                  <input className='mb-1' style={{ width: '80%' }} tabIndex={parseInt(index + 1)} placeholder='1st Pick' onChange={onChange} data-index={1} player-index={index + 1} value={player[1]} onFocus={handleFocus}></input>
+                  <input className='mb-1' style={{ width: '80%' }} tabIndex={(2 * winners) - parseInt(index)} placeholder='2nd Pick' onChange={onChange} data-index={2} player-index={index + 1} value={player[2]} onFocus={handleFocus} ></input>
+                  <input className='mb-1' style={{ width: '80%' }} tabIndex={(3 - 1) * winners + index + 1} placeholder="3rd Pick" onChange={onChange} data-index={3} player-index={index + 1} value={player[3]} onFocus={handleFocus}></input>
+                  <input className='mb-1' style={{ width: '80%' }} tabIndex={(4 * winners) - parseInt(index)} placeholder='4th Pick' onChange={onChange} data-index={4} player-index={index + 1} value={player[4]} onFocus={handleFocus}></input>
+                  <input className='mb-1' style={{ width: '80%' }} tabIndex={(5 - 1) * winners + index + 1} placeholder="5th Pick" onChange={onChange} data-index={5} player-index={index + 1} value={player[5]} onFocus={handleFocus}></input>
+                  <input className='mb-1' style={{ width: '80%' }} tabIndex={(6 * winners) - parseInt(index)} placeholder='6th Pick' onChange={onChange} data-index={6} player-index={index + 1} value={player[6]} onFocus={handleFocus}></input>
+                  <input className='mb-1' style={{ width: '80%' }} tabIndex={(7 - 1) * winners + index + 1} placeholder="7th Pick" onChange={onChange} data-index={7} player-index={index + 1} value={player[7]} onFocus={handleFocus}></input>
+                  <input className='mb-1' style={{ width: '80%' }} tabIndex={(8 * winners) - parseInt(index)} placeholder="8th Pick" onChange={onChange} data-index={8} player-index={index + 1} value={player[8]} onFocus={handleFocus}></input>
+                  <input className='mb-1' style={{ width: '80%' }} tabIndex={(9 - 1) * winners + index + 1} placeholder="9th Pick" onChange={onChange} data-index={9} player-index={index + 1} value={player[9]} onFocus={handleFocus}></input>
+                  <input className='mb-1' style={{ width: '80%' }} tabIndex={(10 * winners) - parseInt(index)} placeholder="10th Pick" onChange={onChange} data-index={10} player-index={index + 1} value={player[10]} onFocus={handleFocus}></input>
+                  <textarea style={{ width: '80%' }} rows="8" placeholder='Additional Notes' onChange={onChange} data-index={11} player-index={index + 1} value={player[11]} onFocus={handleFocus}></textarea>
                 </form>
               </div>);
 
             })
           }
       </div>
-      <CSVLink data={picks} className='btn btn-success'>Download CSV<i className='fa-solid fa-file-arrow-down ms-2'></i></CSVLink>
+      <div className='mb-5'>
+        <Button variant='success' className='me-2' onClick={handleShowCSV}>Download CSV<i className='fa-solid fa-file-arrow-down ms-2'></i></Button>
+        <Button variant='primary' onClick={handleShow}>Start new hit draft</Button>
+      </div>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Start new hit draft</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Starting a new hit draft will erase the current hit draft. Download the current one if you want to save it.</Modal.Body>
+        <Modal.Footer className='d-flex justify-content-evenly'>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleReset}>
+            Start new draft
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <Modal show={showCSV} onHide={handleCloseCSV}>
+        <Modal.Header closeButton>
+          <Modal.Title>Download CSV</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className='d-flex justify-content-center'><input placeholder='Enter file name' className='col-8' onChange={fileNameChange}></input></Modal.Body>
+        <Modal.Footer className='d-flex justify-content-evenly'>
+          <Button variant="secondary" onClick={handleCloseCSV}>
+            Close
+          </Button>
+          <CSVLink data={picks} filename={fileName} className='btn btn-success me-2'>Download CSV<i className='fa-solid fa-file-arrow-down ms-2'></i></CSVLink>
+        </Modal.Footer>
+      </Modal>
       </>
   );
 }
